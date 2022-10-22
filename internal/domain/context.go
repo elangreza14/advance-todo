b05b8d71f9aed1d4952ab09b1c0213e4de78f7df
elangreza14 <rezaelangerlangga14@gmail.com>
2022-10-22 21:21:12 +0700

    implement-va;idator in handler

--- internal/domain/context.go
+++ internal/domain/context.go
@@ -1,10 +1,15 @@
 package domain
 
 type (
+	// ContextValue is type context
+	// that used for naming context value
 	ContextValue string
 )
 
 const (
-	ContextValueIP     ContextValue = "context-ip"
+	// ContextValueIP is type context for holding ip user
+	ContextValueIP ContextValue = "context-ip"
+
+	// ContextValueUserID is type context for holding user id
 	ContextValueUserID ContextValue = "context-user-id"
 )
--- internal/domain/token.go
+++ internal/domain/token.go
@@ -10,8 +10,11 @@ import (
 )
 
 type (
+	// TokenType is kind of token to save in db and cache
 	TokenType string
-	Token     struct {
+
+	// TokenType is domain for handling all token
+	Token struct {
 		ID        uuid.UUID
 		UserID    uuid.UUID
 		Token     string
@@ -23,6 +26,7 @@ type (
 		Versioning
 	}
 
+	// TokenRepository consist all token behavior of Token
 	TokenRepository interface {
 		GetTokenByID(ctx context.Context, id uuid.UUID) (*Token, error)
 		GetTokenByUserIDAndIP(ctx context.Context, userId uuid.UUID, ip string, tokenType TokenType) (*Token, error)
@@ -31,11 +35,17 @@ type (
 )
 
 const (
+	// TokenTypePassword is type token for changing password
 	TokenTypePassword TokenType = "PASSWORD"
-	TokenTypeAccess   TokenType = "ACCESS"
-	TokenTypeRefresh  TokenType = "REFRESH"
+
+	// TokenTypeAccess is type token for accessing API
+	TokenTypeAccess TokenType = "ACCESS"
+
+	// TokenTypeRefresh is type token for refreshing Token API
+	TokenTypeRefresh TokenType = "REFRESH"
 )
 
+// NewToken is constructor for Token
 func NewToken(gen config.TokenGenerator, req User, tokenType TokenType, ip string) *Token {
 	return &Token{
 		ID:        gen.ID,
